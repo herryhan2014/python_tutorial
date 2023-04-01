a12a5af163e3162802035cd60d0697367ae7cde8
zunyinmon <ymonzun@gmail.com>
2023-04-01 22:03:20 +0800

    Add variable

---
+++ 0_variable.py
@@ -0,0 +1,19 @@
+'''
+python
+training
+'''
+
+#python 
+
+#variable
+'''
+int
+char, str
+float - decimal
+array - list, set, dic, turple
+boorean - true/fasle , 0/1
+condition -  if () else, if() elif() elif() elif() else
+loop - for/while
+function - build-in, main
+class - OOP
+'''
